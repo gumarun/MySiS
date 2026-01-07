6b48292aad9cdc070c7d9b78cd5d11b6f9ce887c
gumaru <108120895+gumarun@users.noreply.github.com>
2026-01-07 13:10:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 
         st.subheader(fruit_chosen + 'Nutrition Infomaion')
+        st.write("DEBUG search_on =", repr(search_on))
         smoothiefroot_response = requests.get(
             f"https://my.smoothiefroot.com/api/fruit/{search_on}"
         )
